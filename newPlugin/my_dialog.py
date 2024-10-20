31722cc3f0dedae3b78639c0c1a681549fdf56af
N.Minayev <minyaev@jivosite.com>
2024-10-21 00:19:17 +0400

    рефакторинг фронта

--- newPlugin/my_dialog.py
+++ newPlugin/my_dialog.py
@@ -40,87 +40,76 @@ def send_get_request(url_template: str, path_params: dict = None, query_params: 
 class MyDialog(QtWidgets.QDialog):
     def __init__(self):
         super(MyDialog, self).__init__()
-        self.setWindowTitle('Search Part by Name')
-        self.setGeometry(100, 100, 400, 300)
+        self.setWindowTitle('Search Obj by Name')
+        self.setGeometry(100, 100, 500, 150)  # Adjusted width and reduced height
 
-        # Поле для ввода имени
-        self.label = QtWidgets.QLabel('Enter Part Name:', self)
-        self.label.setGeometry(QtCore.QRect(20, 20, 100, 30))
+        layout = QtWidgets.QVBoxLayout(self)
 
-        self.textInput = QtWidgets.QLineEdit(self)
-        self.textInput.setGeometry(QtCore.QRect(120, 20, 200, 30))
-
-        # Кнопка для поиска
-        self.submitButton = QtWidgets.QPushButton('Search', self)
-        self.submitButton.setGeometry(QtCore.QRect(150, 70, 100, 30))
+        # Search input area
+        search_layout = QtWidgets.QHBoxLayout()
+        self.label = QtWidgets.QLabel('Enter Obj Name:')
+        self.textInput = QtWidgets.QLineEdit()
+        self.submitButton = QtWidgets.QPushButton('Search')
         self.submitButton.clicked.connect(self.search_part)
 
-        # Поля для отображения найденной информации о детали
-        self.nameLabel = QtWidgets.QLabel('Part Name:', self)
-        self.nameLabel.setGeometry(QtCore.QRect(20, 120, 100, 30))
-        self.nameOutput = QtWidgets.QLabel(self)
-        self.nameOutput.setGeometry(QtCore.QRect(120, 120, 200, 30))
+        search_layout.addWidget(self.label)
+        search_layout.addWidget(self.textInput)
+        search_layout.addWidget(self.submitButton)
 
-        self.idLabel = QtWidgets.QLabel('Part ID:', self)
-        self.idLabel.setGeometry(QtCore.QRect(20, 160, 100, 30))
-        self.idOutput = QtWidgets.QLabel(self)
-        self.idOutput.setGeometry(QtCore.QRect(120, 160, 200, 30))
+        layout.addLayout(search_layout)
 
-        # Кнопка для действия с найденной деталью (например, передача id)
-        self.actionButton = QtWidgets.QPushButton('Load', self)
-        self.actionButton.setGeometry(QtCore.QRect(150, 220, 100, 30))
+        # Results area (initially hidden)
+        self.results_widget = QtWidgets.QWidget()
+        results_layout = QtWidgets.QHBoxLayout(self.results_widget)
+
+        self.nameLabel = QtWidgets.QLabel('Obj Name:')
+        self.nameOutput = QtWidgets.QLabel()
+        self.idLabel = QtWidgets.QLabel('Obj ID:')
+        self.idOutput = QtWidgets.QLabel()
+        self.actionButton = QtWidgets.QPushButton('Load')
         self.actionButton.clicked.connect(self.load_object)
-        self.actionButton.setEnabled(False)  # Отключаем кнопку, пока не найдена деталь
+
+        results_layout.addWidget(self.nameLabel)
+        results_layout.addWidget(self.nameOutput)
+        results_layout.addWidget(self.idLabel)
+        results_layout.addWidget(self.idOutput)
+        results_layout.addWidget(self.actionButton)
+
+        layout.addWidget(self.results_widget)
+
+        # Initially hide the results
+        self.results_widget.hide()
 
         # Храним ID детали для дальнейшего использования
         self.part_id = None
 
     def search_part(self):
-        # Получаем данные из формы
         part_name = self.textInput.text()
-
-        # Проверка, что поле ввода не пустое
         if not part_name:
             QtWidgets.QMessageBox.warning(self, 'Warning', 'Please enter a part name!')
             return
 
-        # Пример отправки GET-запроса для поиска детали по имени
         try:
             query_params = {"name": part_name}
             response = send_get_request("/api/basic_object", query_params=query_params)
-
-            # Парсим ответ
             data = json.loads(response)
-            print(data)
-            # Проверяем, найдено ли что-то
-            if data:
-                # file_path = data['basic_object']['bounding_contour']['brep_files']['path']
-                # Извлекаем имя и id
-                part_name = data['basic_object']['name'] # get('name')
-                part_id = data['basic_object']['id']
 
-                # Отображаем данные на интерфейсе
+            if data and 'basic_object' in data:
+                part_name = data['basic_object'].get('name', 'N/A')
+                part_id = data['basic_object'].get('id', 'N/A')
+
                 self.nameOutput.setText(part_name)
-                self.idOutput.setText(part_id)
-
-                # Сохраняем id для дальнейшего использования
+                self.idOutput.setText(str(part_id))
                 self.part_id = part_id
 
-                # Включаем кнопку для действия
-                self.actionButton.setEnabled(True)
-
-                # print(file_path)
-                # if file_path:
-                #     QtWidgets.QMessageBox.information(self, 'Success', f'Part found: {file_path}')
-                #     # Можно открыть файл в FreeCAD, как показано в вашем примере
-                #     import FreeCAD
-                #     FreeCAD.open(file_path)
-                # else:
-                #     QtWidgets.QMessageBox.critical(self, 'Error', 'Object found, but no file path available!')
+                # Show the results
+                self.results_widget.show()
             else:
                 QtWidgets.QMessageBox.critical(self, 'Error', 'No objects found with this name!')
+                self.results_widget.hide()
         except Exception as e:
             QtWidgets.QMessageBox.critical(self, 'Error', f'An error occurred: {e}')
+            self.results_widget.hide()
 
     def load_object(self):
         # Проверяем, есть ли id детали
